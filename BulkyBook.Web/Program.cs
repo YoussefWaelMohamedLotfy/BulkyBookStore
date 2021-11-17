60c472ff8cab36845fe273bed62601391671bd85
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-11-18 01:53:26 +0200

    Completed Section 2

--- BulkyBook.Web/Program.cs
+++ BulkyBook.Web/Program.cs
@@ -1,6 +1,11 @@
+using BulkyBook.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
